513751f589f4fe4c48667e7d96267b735b9cf884
Mukur Panchani <panchanimukur@gmail.com>
2019-04-15 13:17:34 +0530

    Add modified logic a/c to backend

--- app/src/main/java/Subject.java
+++ app/src/main/java/Subject.java
@@ -1,4 +1,86 @@
-package PACKAGE_NAME;
-
 public class Subject {
+
+    private String mAcadYear;
+    private String mAttendancePercentage;
+    private String mDaysAbsent;
+    private String mDaysPresent;
+    private String mTotalClasses;
+    private String mSemester;
+    private String mSubjectName;
+    private String mSubjectCode;
+
+    public Subject(String mAcadYear, String mAttendancePercentage, String mDaysAbsent, String mDaysPresent, String mTotalClasses, String mSemester, String mSubjectName, String mSubjectCode) {
+        this.mAcadYear = mAcadYear;
+        this.mAttendancePercentage = mAttendancePercentage;
+        this.mDaysAbsent = mDaysAbsent;
+        this.mDaysPresent = mDaysPresent;
+        this.mTotalClasses = mTotalClasses;
+        this.mSemester = mSemester;
+        this.mSubjectName = mSubjectName;
+        this.mSubjectCode = mSubjectCode;
+    }
+
+    public String getmAcadYear() {
+        return mAcadYear;
+    }
+
+    public void setmAcadYear(String mAcadYear) {
+        this.mAcadYear = mAcadYear;
+    }
+
+    public String getmAttendancePercentage() {
+        return mAttendancePercentage;
+    }
+
+    public void setmAttendancePercentage(String mAttendancePercentage) {
+        this.mAttendancePercentage = mAttendancePercentage;
+    }
+
+    public String getmDaysAbsent() {
+        return mDaysAbsent;
+    }
+
+    public void setmDaysAbsent(String mDaysAbsent) {
+        this.mDaysAbsent = mDaysAbsent;
+    }
+
+    public String getmDaysPresent() {
+        return mDaysPresent;
+    }
+
+    public void setmDaysPresent(String mDaysPresent) {
+        this.mDaysPresent = mDaysPresent;
+    }
+
+    public String getmTotalClasses() {
+        return mTotalClasses;
+    }
+
+    public void setmTotalClasses(String mTotalClasses) {
+        this.mTotalClasses = mTotalClasses;
+    }
+
+    public String getmSemester() {
+        return mSemester;
+    }
+
+    public void setmSemester(String mSemester) {
+        this.mSemester = mSemester;
+    }
+
+    public String getmSubjectName() {
+        return mSubjectName;
+    }
+
+    public void setmSubjectName(String mSubjectName) {
+        this.mSubjectName = mSubjectName;
+    }
+
+    public String getmSubjectCode() {
+        return mSubjectCode;
+    }
+
+    public void setmSubjectCode(String mSubjectCode) {
+        this.mSubjectCode = mSubjectCode;
+    }
 }
