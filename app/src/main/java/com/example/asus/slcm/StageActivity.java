7d15a316ecc0070e4fe7df17ac5d99436410cd96
Mukur Panchani <panchanimukur@gmail.com>
2019-04-16 17:32:36 +0530

    Attempt Recycler View inflation

--- app/src/main/java/com/example/asus/slcm/StageActivity.java
+++ app/src/main/java/com/example/asus/slcm/StageActivity.java
@@ -2,24 +2,32 @@ package com.example.asus.slcm;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.res.Resources;
 import android.graphics.Color;
+import android.graphics.Rect;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.CardView;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.util.TypedValue;
 import android.view.Gravity;
+import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import java.util.List;
+
 public class StageActivity extends AppCompatActivity {
 
-    CardView cardview;
-    LinearLayout.LayoutParams layoutparams;
-    TextView textview;
     RelativeLayout relativeLayout;
     Context context;
+    private RecyclerView recyclerView;
+    private SubjectRecyclerAdapter srAdapter;
+    private List<Subject> subjectList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,7 +41,53 @@ public class StageActivity extends AppCompatActivity {
         User ourBoi = (User) i.getSerializableExtra("current_user");
         Log.d("BOI", ourBoi.getmRawPassword());
         if (ourBoi.getAreSubjectsLoaded()) {
-
+            recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+            srAdapter = new SubjectRecyclerAdapter(this, (List<Subject>) ourBoi.subjectList);
+//            RecyclerView.LayoutManager mLayoutManager = new GridLayoutManager(this, 2);
+//            recyclerView.setLayoutManager(mLayoutManager);
+//            recyclerView.addItemDecoration(new GridSpacingItemDecoration(2, dpToPx(10), true));
+//            recyclerView.setItemAnimator(new DefaultItemAnimator());
+            recyclerView.setAdapter(srAdapter);
         }
+    }
+
+    public class GridSpacingItemDecoration extends RecyclerView.ItemDecoration {
+
+        private int spanCount;
+        private int spacing;
+        private boolean includeEdge;
+
+        public GridSpacingItemDecoration(int spanCount, int spacing, boolean includeEdge) {
+            this.spanCount = spanCount;
+            this.spacing = spacing;
+            this.includeEdge = includeEdge;
+        }
+
+        @Override
+        public void getItemOffsets(Rect outRect, View view, RecyclerView parent, RecyclerView.State state) {
+            int position = parent.getChildAdapterPosition(view); // item position
+            int column = position % spanCount; // item column
+
+            if (includeEdge) {
+                outRect.left = spacing - column * spacing / spanCount; // spacing - column * ((1f / spanCount) * spacing)
+                outRect.right = (column + 1) * spacing / spanCount; // (column + 1) * ((1f / spanCount) * spacing)
+
+                if (position < spanCount) { // top edge
+                    outRect.top = spacing;
+                }
+                outRect.bottom = spacing; // item bottom
+            } else {
+                outRect.left = column * spacing / spanCount; // column * ((1f / spanCount) * spacing)
+                outRect.right = spacing - (column + 1) * spacing / spanCount; // spacing - (column + 1) * ((1f /    spanCount) * spacing)
+                if (position >= spanCount) {
+                    outRect.top = spacing; // item top
+                }
+            }
+        }
+    }
+
+    private int dpToPx(int dp) {
+        Resources r = getResources();
+        return Math.round(TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, r.getDisplayMetrics()));
     }
 }
---
+++ app/src/main/java/com/example/asus/slcm/SubjectRecyclerAdapter.java
@@ -0,0 +1,56 @@
+package com.example.asus.slcm;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import java.util.List;
+
+public class SubjectRecyclerAdapter extends RecyclerView.Adapter<SubjectRecyclerAdapter.SubjectViewHolder> {
+
+    private Context mContext;
+    private List<Subject> mSubjectList;
+
+    public class SubjectViewHolder extends RecyclerView.ViewHolder {
+        public TextView subjectName, atdPer, totalClasses, presentClasses, bunkedClasses;
+
+        public SubjectViewHolder(View view) {
+            super(view);
+            subjectName = (TextView) view.findViewById(R.id.subjectName);
+            atdPer = (TextView) view.findViewById(R.id.atdPer);
+            totalClasses = (TextView) view.findViewById(R.id.totalClasses);
+            presentClasses = (TextView) view.findViewById(R.id.presentClasses);
+            bunkedClasses = (TextView) view.findViewById(R.id.bunkedClasses);
+        }
+    }
+
+    public SubjectRecyclerAdapter(Context mContext, List<Subject> mSubjectList) {
+        this.mContext = mContext;
+        this.mSubjectList = mSubjectList;
+    }
+
+    @Override
+    public SubjectViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.rview_item, parent, false);
+        return new SubjectViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(final SubjectViewHolder holder, int position) {
+        Subject subject = mSubjectList.get(position);
+        holder.subjectName.setText(subject.getmSubjectName());
+        holder.atdPer.setText(subject.getmAttendancePercentage() + " %");
+        holder.presentClasses.setText(subject.getmDaysPresent());
+        holder.bunkedClasses.setText(subject.getmDaysAbsent());
+        holder.totalClasses.setText(subject.getmTotalClasses());
+    }
+
+    @Override
+    public int getItemCount() {
+        return mSubjectList.size();
+    }
+
+}
