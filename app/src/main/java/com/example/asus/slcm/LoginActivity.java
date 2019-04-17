845187d64b37da718c898bf91a156beb8ac9fa48
Mukur Panchani <panchanimukur@gmail.com>
2019-04-17 07:11:06 +0530

    Improve robustness of incorrect entries

--- app/src/main/java/com/example/asus/slcm/LoginActivity.java
+++ app/src/main/java/com/example/asus/slcm/LoginActivity.java
@@ -2,6 +2,7 @@ package com.example.asus.slcm;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.CardView;
@@ -11,6 +12,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 
@@ -25,6 +27,9 @@ import com.android.volley.toolbox.Volley;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.w3c.dom.Text;
+
+import java.util.regex.Pattern;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -35,6 +40,9 @@ public class LoginActivity extends AppCompatActivity {
     private String mPassword;
     public User ourBoi = new User();
     private Context mContext;
+    LinearLayout linlaHeaderProgress;
+    CardView cv;
+    TextView appTitle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,27 +54,30 @@ public class LoginActivity extends AppCompatActivity {
         loginButton=findViewById(R.id.loginButton);
         mQueue = Volley.newRequestQueue(this);
         mContext = getApplicationContext();
+        linlaHeaderProgress = (LinearLayout) findViewById(R.id.linlaHeaderProgress);
+        cv = (CardView) findViewById(R.id.card_view_login);
+        appTitle = (TextView) findViewById(R.id.app_title);
 
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
                  String user=userName.getText().toString().trim();
                  String pass=password.getText().toString().trim();
-
-
-                if ( !(TextUtils.isEmpty(user)) && !(TextUtils.isEmpty(pass)) ){
-
-                    /*write the code for login that is your http request*/
-
-                    mUsername = user;
-                    mPassword = pass;
-                    ourBoi.setmRegistrationNumber(mUsername);
-                    ourBoi.setmRawPassword(mPassword);
-                    jsonParse();
-                } else {
-                    Toast.makeText(LoginActivity.this, "Please enter username or password", Toast.LENGTH_LONG).show();
-                }
+                 if (!(TextUtils.isEmpty(user)) && !(TextUtils.isEmpty(pass))) {
+                     if (verifyUserName(user)) {
+                         Log.i("verify", "SUCCC");
+                         mUsername = user;
+                         mPassword = pass;
+                         ourBoi.setmRegistrationNumber(mUsername);
+                         ourBoi.setmRawPassword(mPassword);
+                         jsonParse();
+                     } else {
+                         Log.i("verify", "UN ____  SUCCC");
+                         Toast.makeText(LoginActivity.this, "Invalid Registration Number", Toast.LENGTH_LONG).show();
+                     }
+                 } else {
+                     Toast.makeText(LoginActivity.this, "Please enter username or password", Toast.LENGTH_LONG).show();
+                 }
             }
         });
     }
@@ -74,57 +85,87 @@ public class LoginActivity extends AppCompatActivity {
     private void jsonParse() {
         String url = "http://13.234.66.100/go?username=" + mUsername + "&password=" + mPassword;
 
-        final LinearLayout linlaHeaderProgress = (LinearLayout) findViewById(R.id.linlaHeaderProgress);
-        final CardView cv = (CardView) findViewById(R.id.card_view_login);
-        cv.setVisibility(View.GONE);
-        linlaHeaderProgress.setVisibility(View.VISIBLE);
+        progressBarStart();
 
         final JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null,
                 new Response.Listener<JSONObject>() {
                     @Override
                     public void onResponse(JSONObject response) {
                         try {
+                            Log.d("res", response.toString());
                             if (response.getString("code").equals("666")) { //Successful login
                                 // parse subject data
                                 JSONArray subjectArray = response.getJSONArray("data");
-                                for (int i = 0; i < subjectArray.length(); i++) {
-                                    JSONObject subjectObject = subjectArray.getJSONObject(i);
-                                    String a = subjectObject.getString("Academic Year");
-                                    String b = subjectObject.getString("Attendance(%)");
-                                    String c = subjectObject.getString("Days Absent");
-                                    String d = subjectObject.getString("Days Present");
-                                    String e = subjectObject.getString("Total Class");
-                                    String f = subjectObject.getString("Semester");
-                                    String g = subjectObject.getString("Subject ");
-                                    String h = subjectObject.getString("Subject Code");
-                                    Subject sub = new Subject(a, b, c, d, e, f, g, h);
-                                    ourBoi.addSubject(sub);
+                                if (subjectArray.length() > 0) {
+                                    for (int i = 0; i < subjectArray.length(); i++) {
+                                        JSONObject subjectObject = subjectArray.getJSONObject(i);
+                                        String a = subjectObject.getString("Academic Year");
+                                        String b = subjectObject.getString("Attendance(%)");
+                                        String c = subjectObject.getString("Days Absent");
+                                        String d = subjectObject.getString("Days Present");
+                                        String e = subjectObject.getString("Total Class");
+                                        String f = subjectObject.getString("Semester");
+                                        String g = subjectObject.getString("Subject ");
+                                        String h = subjectObject.getString("Subject Code");
+                                        Subject sub = new Subject(a, b, c, d, e, f, g, h);
+                                        ourBoi.addSubject(sub);
+                                    }
+                                    if (ourBoi.getAreSubjectsLoaded()) {
+                                        Toast.makeText(LoginActivity.this, "Success :)", Toast.LENGTH_LONG).show();
+                                        SharedPreferences sharedPref = LoginActivity.this.getPreferences(Context.MODE_PRIVATE);
+                                        SharedPreferences.Editor editor = sharedPref.edit();
+                                        editor.putString("tentative_username", userName.getText().toString().trim());
+                                        editor.putString("tentative_password", password.getText().toString().trim());
+                                        editor.apply();
+                                        Intent i = new Intent(mContext, StageActivity.class);
+                                        i.putExtra("current_user", ourBoi);
+                                        startActivity(i);
+                                    }
+                                } else {
+                                    progressBarEnd();
+                                    Toast.makeText(LoginActivity.this,"Incorrect Credentials :(\nCODE: ", Toast.LENGTH_LONG).show();
                                 }
-                                cv.setVisibility(View.VISIBLE);
-                                linlaHeaderProgress.setVisibility(View.GONE);
-                                Intent i = new Intent(mContext, StageActivity.class);
-                                i.putExtra("current_user", ourBoi);
-                                startActivity(i);
                             } else {
                                 //erroneous login
+                                progressBarEnd();
                                 Toast.makeText(LoginActivity.this,"ERROR :(\nCODE: " + response.getString("code"),Toast.LENGTH_LONG).show();
                             }
 
                         } catch (JSONException e) {
                             Log.e("JSON ERROR", e.toString());
                             e.printStackTrace();
-                        } finally {
-                            linlaHeaderProgress.setVisibility(View.GONE);
                         }
                     }
 
                 }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
+                Log.e("SOMEERROR", "Well this is a bust." + error.toString());
                 error.printStackTrace();
             }
         });
         mQueue.add(request);
     }
 
+    private void progressBarStart() {
+        cv.setVisibility(View.GONE);
+        appTitle.setVisibility(View.GONE);
+        linlaHeaderProgress.setVisibility(View.VISIBLE);
+    }
+
+    private void progressBarEnd() {
+        cv.setVisibility(View.VISIBLE);
+        appTitle.setVisibility(View.VISIBLE);
+        linlaHeaderProgress.setVisibility(View.GONE);
+    }
+
+    public boolean verifyUserName(String user) {
+        return user.matches("[12][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        progressBarEnd();
+    }
 }
